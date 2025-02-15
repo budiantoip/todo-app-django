09f27e534799c399606034cd99613baea3deb320
Budianto IP <budiantoip@gmail.com>
2025-02-15 09:40:18 +0700

    Init database

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'todo_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo_db',
+        'USER': 'your_username',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
